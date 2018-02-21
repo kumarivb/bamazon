0a0feff41d99da9d7e15b69785c9d65460696f6e
kvb <kumarivb@gmail.com>
2018-02-21 16:47:13 -0600

    added function to bamazonCust.js to show table

--- bamazonCust.js
+++ bamazonCust.js
@@ -14,5 +14,19 @@ var connection = mysql.createConnection({
 // connect to mysql server and sql database
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("you are connected!");
+    // console.log("you are connected!");
+
+    // show database products
+    showTable();
 });
+
+function showTable() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
+        } 
+        console.log("-------------------------------------------");
+    });
+}
+
+
